12d4f55e9d9563766b42ca6b3a1cec19010744f1
Rodrigo Reis de Sousa <32500158+RodrigoRsousa@users.noreply.github.com>
2021-11-22 21:26:15 -0300

    Add files via upload

---
+++ src/routes/musics.js
@@ -0,0 +1,69 @@
+const express = require('express');
+const router = express.Router();
+
+// These routes serves Musics
+
+let musics = [
+    {
+        name: 'Falling',
+        artist: 'Léon',
+        duration: '3:55',
+        year: 2019
+    },
+];
+
+// Check if the song name is inside Body
+
+function checkMusicExists (req, res, next) {
+    if (!req.body.name || !req.body.artist || !req.body.duration || !req.body.year) {
+    return res.status(400).json({ error: 'I dont accept empty fields' });   
+    }
+    return next(); 
+}
+
+// Check if the song id exists in my array
+
+function checkMusicInArray(req, res, next) {
+    const music = musics[req.params.id];
+    if (!music) {
+        return res.status(404).json({ error: 'Music does not exists' });
+    } 
+    req.music = music;
+    return next();
+}
+
+// Starting Endpoint routing
+
+router.get ('/', (req,res) => {
+    res.status(200).send({  message: 'Welcome, we have these songs with us: ', musics });
+});
+
+router.get ('/:id', checkMusicInArray, (req,res) => {
+    const { id } = req.params;
+    const music = musics[id];
+
+    res.status(200).send({ music });
+});
+
+router.post('/', checkMusicExists, (req, res) => {
+    const name = req.body;
+    musics.push(name);
+
+    res.status(200).json({ message: 'Song successfully created!', details: musics, redirect: '/musics/' });
+});
+
+router.put("/:id", checkMusicInArray, checkMusicExists, (req, res) => {
+    const { id } = req.params;
+    musics[id] = req.body;
+
+    res.status(200).json({ message: `Song ${musics[id].name} from ${musics[id].artist} successfully modified!`, details: musics[id], redirect: '/musics/:id' });
+});
+
+router.delete('/:id', checkMusicInArray, (req, res) => {
+    const { id } = req.params;
+    musics.splice(id, 1);
+
+    res.status(200).send({ message: 'Song Successfully Deleted!', redirect: '/musics/' });
+});
+
+module.exports = router;
---
+++ src/routes/recipes.js
@@ -0,0 +1,71 @@
+const express = require('express');
+const router = express.Router();
+
+// These routes serves recipes (ideia from last project)
+
+let recipes = [
+    {
+    name: 'Rabanada',
+    cuisine: 'Brazilian French Toast',
+    history: 'Rabanada is the Brazilian version of french toast.',
+    image: 'https://cdn.tasteatlas.com/images/dishes/5c33b6059a634e6195f80ff37c3f0997.jpg?mw=1300'
+    },
+];
+
+// Rabanada is the BEST, add some banana with it. =D
+
+router.get ('/', (req,res) => {
+    res.status(200).send({ recipes, message: 'Welcome' });
+});
+
+router.get ('/:id', (req,res) => {
+    const id = req.params.id;
+    const recipe = recipes[id];
+    
+    if (!recipe) {
+        res.status(404).send(`Recipe with id ${id} was not found` );
+    } else {
+        res.status(200).send({ recipe });
+    }
+});
+
+router.post('/', (req, res) => {
+    const recipe = req.body;
+    const id = recipes.length;
+
+    if(!req.body.name || !req.body.cuisine || !req.body.history || !req.body.image) {
+        res.status(400).json({ error: 'I dont accept empty fields' });   
+    };
+
+    recipes.push(recipe);
+    res.status(200).send({ message: `Recipe Successfully added! Recipe ID: ${id}` });
+});
+
+router.put("/:id", (req, res) => {
+    const id = req.params.id;
+    const recipe = req.body;
+    const false_id = recipes[id];
+
+    if ( !false_id ) { 
+        res.status(404).send(`Recipe with id ${id} was not found` );
+    } if (!req.body.name || !req.body.cuisine || !req.body.history || !req.body.image) {
+        res.status(400).json({ error: 'I dont accept empty fields' });   
+    } else {
+        recipes[id] = recipe;
+        res.status(200).send({ message: `Recipe Successfully Modified! Recipe ID: ${id}` });
+    }
+});
+
+router.delete('/:id', (req, res) => {
+    const id = req.params.id;
+    const false_id = recipes[id];
+
+    if( !false_id ) { 
+        res.status(404).send(`Recipe with id ${id} was not found` );
+    } else {
+        delete recipes.splice(id, 1); 
+        res.status(200).send({ message: `Recipe Successfully Deleted! Recipe ID: ${id}` });
+    }
+});
+
+module.exports = router;
