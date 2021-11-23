a3470f9d200d1a92add38dd296f8b375a4f8d137
Rodrigo Reis de Sousa <32500158+RodrigoRsousa@users.noreply.github.com>
2021-11-22 21:26:36 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+app.get('/', (req,res) => {
+    res.status(200).send({ message: 'Welcome to my Humble Home. You can go to: /recipes or /musics' }); 
+});
+
+const musicsRouter = require('./src/routes/musics'); 
+app.use('/musics', musicsRouter); 
+
+const recipesRouter = require('./src/routes/recipes');
+app.use('/recipes', recipesRouter);
+
+app.listen(port, () => console.info(`Server listening at http://localhost:${port}`));
